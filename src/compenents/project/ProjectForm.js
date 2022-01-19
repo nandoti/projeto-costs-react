dd2300d0bc68384015a563d996e3d09228e9e3c2
nandoti <fernando.adriano.tavares@gmail.com>
2022-01-18 22:21:45 -0300

    :construction:  Criada API e conectando ao projeto

--- src/compenents/project/ProjectForm.js
+++ src/compenents/project/ProjectForm.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/Submit";
@@ -5,6 +7,22 @@ import SubmitButton from "../form/Submit";
 import styles from "./ProjectForm.module.css";
 
 function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((error) => console.log(error));
+  },[])
+
   return (
     <form className={styles.form}>
       <Input
@@ -19,7 +37,7 @@ function ProjectForm({ btnText }) {
         name="budget"
         placeholder="Insira o orçamento total"
       />
-      <Select name="category_id" text="Selecione a categoria" />
+      <Select name="category_id" text="Selecione a categoria" options={categories}/>
       <SubmitButton text={btnText} />
     </form>
   );
