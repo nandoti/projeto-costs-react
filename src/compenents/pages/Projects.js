be385545136c5fc08a4d9eec585d6f0e1464c4a5
nandoti <fernando.adriano.tavares@gmail.com>
2022-02-04 14:42:52 -0300

    :tada: Criando botão para o Dashboard

--- src/compenents/pages/Projects.js
+++ src/compenents/pages/Projects.js
@@ -1,6 +1,10 @@
-import {useLocation} from 'react-router-dom'
+import {Link, useLocation} from 'react-router-dom'
 
 import Message from "../layout/Message";
+import Container from '../layout/Container';
+import LinkButton from '../layout/LinkButton'
+
+import styles from "./Projects.module.css";
 
 function Projects() {
 
@@ -12,10 +16,17 @@ function Projects() {
   }
 
   return (
-    <div>
-      <h1>Meus Projetos</h1>
-      {message && <Message type="success" msg={message}/>}
-    </div>)
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Criar Projeto" />
+      </div>
+      {message && <Message type="success" msg={message} />}
+      <Container custonClass="start">
+        <p>Projetos...</p>
+      </Container>
+    </div>
+  );
 }
 
 export default Projects;
